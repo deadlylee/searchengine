3609ba49a73dfedf89bfe8c9ae5b3488a192ec0b
Timur Li <leeeeeet1967@gmail.com>
2024-09-20 16:38:58 +0500

    modifying annotation added for deleteByUrl method (now able to start indexing more than once)

--- src/main/java/searchengine/repository/SiteRepository.java
+++ src/main/java/searchengine/repository/SiteRepository.java
@@ -1,9 +1,13 @@
 package searchengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import searchengine.model.Site;
 
 public interface SiteRepository extends JpaRepository<Site, Integer> {
 
+    @Modifying()
+    @Transactional
     void deleteByUrl(String url);
 }
